74cf34eb5df1866efb073146609ed3005342116b
leleony <le.leony00@gmail.com>
2021-06-16 15:15:40 +0700

    initial commit

---
+++ src/handler.js
@@ -0,0 +1,121 @@
+const {nanoid} = require('nanoid');
+const notes = require('./notes');
+
+const addNoteHandler = (request, h) => {
+  const {title, tags, body} = request.payload;
+
+  const id = nanoid(16);
+
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {title, tags, body, id, createdAt, updatedAt};
+
+  notes.push(newNote);
+
+  const isSuccess = notes.filter((note)=>(note.id === id)).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Note has been added',
+      data: {
+        noteId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'failed',
+    message: 'Failed to Upload the Notes',
+  });
+  response.code(500);
+  return response;
+};
+
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+
+const getNotesByIdHandler = (request, h) => {
+  const {id} = request.params;
+  const note = notes.filter((n) => n.id === id)[0];
+
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Note Not Found',
+  });
+  response.code(404);
+  return response;
+};
+
+const editNoteById = (request, h) => {
+  const {id} = request.params;
+
+  const {title, tags, body} = request.payload;
+  const updatedAt = new Date().toISOString();
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+    const response = h.response({
+      status: 'success',
+      message: 'The Note Has Been Updated!',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Error in editing note, ID not found',
+  });
+  response.code(404);
+  return response;
+};
+
+const deleteNoteById = (request, h) => {
+  const {id} = request.params;
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes.splice(index,1);
+    const response = h.response({
+      status: 'success',
+      message: 'Note has succesfully been deleted',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Failed deleting notes, ID not found',
+  })
+  response.code(404);
+  return response;
+};
+
+module.exports = {addNoteHandler, getAllNotesHandler, getNotesByIdHandler, editNoteById, deleteNoteById};
